5d8f56f2a1bc465a3710072daaa6a9a0391d98b4
Felipe Martins Abreu <felipeabreu.rj@gmail.com>
2020-10-18 15:54:26 -0300

    Alteração na inicialização do banco de dados na classe App

--- src/app.js
+++ src/app.js
@@ -1,13 +1,14 @@
 import express from 'express';
 import cors from 'cors';
 import routes from './routes';
-import './database';
+import database from './database/index';
 
 class App {
   constructor() {
     this.server = express();
     this.middlewares();
     this.routes();
+    database.init();
   }
 
   middlewares() {
