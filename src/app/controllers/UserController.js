c5e4af443c13d373cb4931adda6cc6ff94c6f9bc
Felipe Martins Abreu <felipeabreu.rj@gmail.com>
2020-10-18 20:10:20 -0300

    Criando as validacoes na controller para o model User

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,7 +1,20 @@
+import * as Yup from 'yup';
 import User from '../models/User';
 
 class UserController {
   async store(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string().email().required(),
+      password_hash: Yup.string().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({
+        error: 'Falha na validação',
+      });
+    }
+
     const userExists = await User.findOne({
       where: { email: req.body.email },
     });
