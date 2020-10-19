d5f2357c1a1e5be30f318b37274e8b3c70eb5d1a
Felipe Martins Abreu <felipeabreu.rj@gmail.com>
2020-10-19 17:11:21 -0300

    Implementação da autenticação JWT

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,14 @@
 import { Router } from 'express';
 
 import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
 
 const routes = new Router();
 
 // User Controller
 routes.post('/users', UserController.store);
+
+// Session Controller
+routes.post('/sessions', SessionController.store);
 
 export default routes;
