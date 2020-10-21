aa73e380c5eb8130a5cceae480b375c7c4c1522b
Felipe Martins Abreu <felipeabreu.rj@gmail.com>
2020-10-21 13:44:57 -0300

    Atualizando perfil do usuario

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -3,7 +3,31 @@ import User from '../models/User';
 
 class UserController {
   async update(req, res) {
-    return res.json({ userId: req.userId });
+    const { email, oldPassword } = req.body;
+
+    const user = await User.findByPk(req.userId);
+
+    if (email !== user.email) {
+      const userExists = await User.findOne({
+        where: { email },
+      });
+
+      if (userExists) {
+        return res.status(400).json({ error: 'Usuario já existe.' });
+      }
+    }
+
+    if (oldPassword && !(await user.checkPassword(oldPassword))) {
+      return res.status(401).json({ error: 'Senha incorreta.' });
+    }
+
+    const { id, name } = await user.update(req.body);
+
+    return res.json({
+      id,
+      name,
+      email,
+    });
   }
 
   async store(req, res) {
