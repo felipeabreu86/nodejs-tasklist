403e7aeb1a982ac39521e692f99862a4b38fc6b8
Felipe Martins Abreu <felipeabreu.rj@gmail.com>
2020-10-22 13:04:21 -0300

    Codificação da atualização e remoção de tarefas

--- src/routes.js
+++ src/routes.js
@@ -2,19 +2,21 @@ import { Router } from 'express';
 import authMiddleware from './app/middlewares/auth';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import TaskController from './app/controllers/TaskController';
 
 const routes = new Router();
 
 // User Controller
 routes.post('/users', UserController.store);
+routes.put('/users', authMiddleware, UserController.update);
 
 // Session Controller
 routes.post('/sessions', SessionController.store);
 
-// Todas rotas abaixo desse middleware precisam estar autenticadas
-routes.use(authMiddleware);
-routes.put('/users', UserController.update);
-// Se eu quisesse passar o middleware individualmente para cada rota, poderia usar assim:
-// routes.put('/users', authMiddleware, UserController.update);
+// Task Controller
+routes.post('/tasks', authMiddleware, TaskController.store);
+routes.get('/tasks', authMiddleware, TaskController.index);
+routes.put('/tasks/:task_id', authMiddleware, TaskController.update);
+routes.delete('/tasks/:task_id', authMiddleware, TaskController.delete);
 
 export default routes;
